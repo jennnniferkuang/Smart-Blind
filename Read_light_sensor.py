597842f19a30ca219b04c99474e2fff8943406c4
jennnniferkuang <94874342+jennnniferkuang@users.noreply.github.com>
2025-09-24 17:43:41 -0400

    read light sensor data

--- Read_light_sensor.py
+++ Read_light_sensor.py
@@ -4,31 +4,41 @@ import time
 import sys
 import os
 import config
+import statistics
 
 libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'TSL2591/lib')
-#print (libdir)
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
 from waveshare_TSL2591 import TSL2591
 
-
+# read values every 5 seconds for 1 minute, then average
 def fnc_collect_avg_values(duration_seconds=5, interval_seconds=2):
     lux_values = []
     infrared_values = []
     visible_values = []
     full_spectrum_values = []
 
-    start_time = time.time()
+    sensor = TSL2591.TSL2591() # init sensor
+    
+    try:
+        for _ in range(12):
+            lux_values.push(sensor.Lux) # all we're using is lux for now but store everything else just in case
 
-    sensor = TSL2591.TSL2591()
+            sensor.TSL2591_SET_LuxInterrupt(50, 200)
+            infrared_values.push(sensor.Read_Infrared)
+            visible_values.push(sensor.Read_Visible)
+            full_spectrum_values.push(sensor.Read_FullSpectrum)
 
+            time.sleep(5)  # Run every 5 seconds
 
+        sensor.Disable() # disable sensor
 
-    sensor.Disable()
-
-
-    return avg_visible
+        return statistics.mean(lux_values)
+    
+    except:
+        sensor.Disable()
+        return None
 
 
 # Example usage:
@@ -38,6 +48,6 @@ if __name__ == "__main__":
             fnc_collect_avg_values(60, 10)   # Collect values for x seconds with y seconds interval
             print("Waiting 5 minutes before next collection...")
             time.sleep(300)  # Sleep for 5 minutes (300 seconds)
-    except KeyboardInterrupt:    
+    except KeyboardInterrupt:
         exit()
 
