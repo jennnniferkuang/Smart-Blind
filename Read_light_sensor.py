05b1a6f16bdac463412a870ba48e916a377661bc
kk k002 <jkuang006@gmail.com>
2025-10-03 10:47:04 -0600

    fix comp errors

--- Read_light_sensor.py
+++ Read_light_sensor.py
@@ -12,8 +12,7 @@ if os.path.exists(libdir):
 
 from waveshare_TSL2591 import TSL2591
 
-# read values every 5 seconds for 1 minute, then average
-def fnc_collect_avg_values(duration_seconds=5, interval_seconds=2):
+def fnc_collect_avg_values(duration_seconds=10, interval_seconds=2):
     lux_values = []
     infrared_values = []
     visible_values = []
@@ -21,33 +20,34 @@ def fnc_collect_avg_values(duration_seconds=5, interval_seconds=2):
 
     sensor = TSL2591.TSL2591() # init sensor
     
-    try:
-        for _ in range(12):
-            lux_values.push(sensor.Lux) # all we're using is lux for now but store everything else just in case
+    while duration_seconds > 0:
+        lux_values.append(sensor.Lux) # all we're using is lux for now but store everything else just in case
+        sensor.TSL2591_SET_LuxInterrupt(50, 200)
 
-            sensor.TSL2591_SET_LuxInterrupt(50, 200)
-            infrared_values.push(sensor.Read_Infrared)
-            visible_values.push(sensor.Read_Visible)
-            full_spectrum_values.push(sensor.Read_FullSpectrum)
+        infrared_values.append(sensor.Read_Infrared)
+        visible_values.append(sensor.Read_Visible)
+        full_spectrum_values.append(sensor.Read_FullSpectrum)
 
-            time.sleep(5)  # Run every 5 seconds
+        duration_seconds -= interval_seconds
+        time.sleep(interval_seconds)
 
-        sensor.Disable() # disable sensor
+    sensor.Disable() # disable sensor
 
-        return statistics.mean(lux_values)
+    return statistics.mean(lux_values)
     
-    except:
-        sensor.Disable()
-        return None
+    # except:
+    #     sensor.Disable()
+    #     return None
 
 
 # Example usage:
 if __name__ == "__main__":
     try:
         while True:
-            fnc_collect_avg_values(60, 10)   # Collect values for x seconds with y seconds interval
-            print("Waiting 5 minutes before next collection...")
-            time.sleep(300)  # Sleep for 5 minutes (300 seconds)
+            avg_val = fnc_collect_avg_values()
+            print(f"Average Lux value over collection period: {avg_val}")
+            print("Waiting 20 secs before next collection...")
+            time.sleep(20)  # Sleep for n seconds
     except KeyboardInterrupt:
         exit()
 
